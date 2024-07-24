23739e0de1857c53ce8747b9f846b772a2a9916c
Victor.X.Qu <fs7744@hotmail.com>
2024-07-24 01:10:25 +0000

    init

---
+++ test/Benchmark/ObjectMappingTest.cs
@@ -0,0 +1,15 @@
+using BenchmarkDotNet.Attributes;
+namespace BenchmarkTest
+{
+    [MemoryDiagnoser]
+    public class ObjectMappingTest
+    { 
+
+        [Benchmark]
+        public void SetClass()
+        {
+
+        }
+
+    }
+}
